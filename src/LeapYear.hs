465505986bdc5959f741105e5b3af5db359b320d
Tim <timothy.d.butterworth@gmail.com>
2017-04-10 06:31:25 -0400

    Changes solution to use foldr

--- src/LeapYear.hs
+++ src/LeapYear.hs
@@ -1,8 +1,11 @@
 module LeapYear (isLeapYear) where
-import Data.List
+
+isN x n = (mod x n) == 0
+
+f4 (_, x) = ((isN x 4), x)
+f100 (b, x) = (b && (not (isN x 100)), x)
+f400 (b, x) = (b || (isN x 400), x)
+identity a = a
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = year `elem` [y | y <- ((fours \\ hundreds) ++ fourhund) ]
-    where fours = [0,4..year]
-          hundreds = [0,100..year]
-          fourhund = [0,400..year]
+isLeapYear year = fst ((foldr (.) identity [f400, f100, f4]) (True, year))
