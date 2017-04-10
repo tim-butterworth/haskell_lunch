9557c5dab3eda27056dfffe66a342918b846663e
Tim <timothy.d.butterworth@gmail.com>
2017-04-10 07:21:28 -0400

    Solves using folr without using composition function

--- src/LeapYear.hs
+++ src/LeapYear.hs
@@ -5,7 +5,8 @@ isN x n = (mod x n) == 0
 f4 (_, x) = ((isN x 4), x)
 f100 (b, x) = (b && (not (isN x 100)), x)
 f400 (b, x) = (b || (isN x 400), x)
-identity a = a
+
+invoker f accume = (f accume)
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = fst ((foldr (.) identity [f400, f100, f4]) (True, year))
+isLeapYear year = fst (foldr invoker (True, year) [f400, f100, f4])
