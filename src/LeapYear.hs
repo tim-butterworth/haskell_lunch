84615976035df23b31db3dbe502d3f61f3843320
Tim <timothy.d.butterworth@gmail.com>
2017-04-10 06:35:22 -0400

    Add solution that uses recursive composition function

--- src/LeapYear.hs
+++ src/LeapYear.hs
@@ -5,7 +5,10 @@ isN x n = (mod x n) == 0
 f4 (_, x) = ((isN x 4), x)
 f100 (b, x) = (b && (not (isN x 100)), x)
 f400 (b, x) = (b || (isN x 400), x)
-identity a = a
+
+compose :: [a -> a] -> a -> a
+compose [x] arg = x arg
+compose (x:xs) arg = x (compose xs arg)
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = fst ((foldr (.) identity [f400, f100, f4]) (True, year))
+isLeapYear year = fst (compose [f400, f100, f4] (True, year))
